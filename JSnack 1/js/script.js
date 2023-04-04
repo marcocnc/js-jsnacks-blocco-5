4cdda9317028ae15dce63a294694c3fe55fb3b6d
marcocnc <marcocancelliere56@gmail.com>
2023-04-04 15:55:36 +0200

    end jsnack 1

---
+++ JSnack 1/js/script.js
@@ -0,0 +1,65 @@
+// Abbiamo un elenco utenti (nome, cognome, età).
+// Trovare i minorenni e gli over 65
+
+// Variabili
+const users = [
+    {
+        name: 'Marco',
+        lastname: 'Cancelliere',
+        age: 23,
+    },
+
+    {
+        name: 'Ciro',
+        lastname: 'Esposito',
+        age: 26,
+    },
+
+    {
+        name: 'Mario',
+        lastname: 'Di Giacomo',
+        age: 16,
+    },
+
+    {
+        name: 'Lorenzo',
+        lastname: 'Lepri',
+        age: 52,
+    },
+
+    {
+        name: 'Nicola',
+        lastname: 'Uccello',
+        age: 80,
+    },
+
+    {
+        name: 'Patrizia',
+        lastname: 'Williams',
+        age: 67,
+    }
+]
+
+
+
+// Evidenziare con filter le varie età
+
+// Minorenni
+const underage = users.filter((underageUser) => {
+    if(underageUser.age < 18){
+        return true;   //! Filter restituisce un valore booleano
+    }
+    return false;
+})
+console.log(underage);
+
+//Over 65
+const over65 = users.filter((over65User) => {
+    if(over65User.age > 65){
+        return true;
+    }
+    return false;
+})
+
+console.log(over65);
+
--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Abbiamo un elenco utenti (nome, cognome, età). <br>
 Trovare i minorenni e gli over 65.
 ### Logica
 1. Creare un array di oggetti contenenti le chiavi nome, cognome ed età;
-2. 
+2. Filter per evidenziare i minorenni e gli over 65
 
 ## Jsnack 2
 Abbiamo un elenco degli studenti di una facoltà. <br>
